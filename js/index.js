2da9793ca6f4a487fc1d0e7857ff8fe3cf85868a
wangfpp <wangfpp@gmail.com>
2019-02-28 18:03:36 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/*
+* @Author: wangfpp
+* @Date:   2019-02-28 11:33:07
+* @Last Modified by:   wangfpp
+* @Last Modified time: 2019-02-28 17:56:18
+*/
+let minimist = require('minimist');
+let argv = minimist(process.argv.slice(2));
+console.log(argv);
+
