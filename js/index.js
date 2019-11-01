16b606824988cca8e0827d62e665e9640a6ed832
wangfpp <wangfpp@gmail.com>
2019-11-01 13:42:01 +0800

    add: get prompt info

--- js/index.js
+++ js/index.js
@@ -3,78 +3,80 @@
 * @Author: wangfpp
 * @Date:   2019-02-28 11:33:07
 * @Last Modified by:   wangfpp
-* @Last Modified time: 2019-10-31 17:57:27
+* @Last Modified time: 2019-11-01 13:39:45
 */
 const fs = require('fs');
-const clear = require('clear');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const package = require('../package.json'); // 引入package项目的相关信息
 const minimist = require('minimist');
 const yargs = require('yargs');
-const {resolve} = require('path')
+const {resolve} = require('path');
+const program = require('commander');
+const inquirer = require('inquirer');
+
+const { getInfo } = require('../lib/userinfo.js'); // 获取当前user信息
 
 let PWD = process.cwd()
 let args = yargs.argv._
 let projectName = args[1];
+let packageJson = {
+	version: "0.0.1"
+}; // package.json的构建信息
 
-if (args.length) {
-	fs.mkdir(`${PWD}/${projectName}`,function(err){
-	   if (err && err.code === 'EEXIST') {
-	       return console.error('目录已经存在');
-	   }
-	   console.log("目录创建成功。");
+// cli argv参数获取
+program
+	.version(package.version, '-v, --version')
+	.description('自定义项目创建cli工具')
+	.option('create,  --init', '初始化项目')
+	.option('-d, --download', '下载一些东西')
+	.parse(process.argv);
 
-	});
-} else {
-	console.log(
-		chalk.black('请输入正确的参数')
-	)
+// prompt Terminal交互获取项目信息
+if (program.init) {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'projectName',
+			message: '请输入项目名称'
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: '项目描述信息'
+		},
+		{
+			type: 'input',
+			name: 'anthor',
+			message: '项目作者',
+			default: () => {
+				return getInfo('git config user.name');
+			}
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: '电子邮箱',
+			default: () => {
+				return getInfo('git config user.email');
+			}
+		},
+		{
+			type: 'list',
+			name: 'type',
+			message: '使用哪种项目模板?',
+			choices: ['Vue', 'React']
+		}
+	]).then(result => {
+		for(let k in result){
+			if (k !== 'type') {
+				packageJson[k] = result[k];
+			}
+		}
+		console.log(packageJson);
+	})
 }
-var copyFolder = function(srcDir, tarDir, cb) {
-  fs.readdir(srcDir, function(err, files) {
-    var count = 0
-    var checkEnd = function() {
-      ++count == files.length && cb && cb()
-    }
 
-    if (err) {
-      checkEnd()
-      return
-    }
-
-    files.forEach(function(file) {
-      var srcPath = path.join(srcDir, file)
-      var tarPath = path.join(tarDir, file)
-
-      fs.stat(srcPath, function(err, stats) {
-        if (stats.isDirectory()) {
-          console.log('mkdir', tarPath)
-          fs.mkdir(tarPath, function(err) {
-            if (err) {
-              console.log(err)
-              return
-            }
-
-            copyFolder(srcPath, tarPath, checkEnd)
-          })
-        } else {
-          copyFile(srcPath, tarPath, checkEnd)
-        }
-      })
-    })
-
-    //为空时直接回调
-    files.length === 0 && cb && cb()
-  })
+if (program.download) {
+	console.log('downloading.....')
 }
-copyFile('../package/' ${PWD}, err => {
-	console.log(err);
-})
-// clear(); // 清空控制台
-// console.log( // 
-// 	chalk.red(
-// 		figlet.textSync(`love❤️wangfpp`, { horizontalLayout: 'full'})
-// 	)
-// );
-// console.log(process.argv);
---
+++ js/install.js
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env node
+/*
+* @descript 插件安装 
+* @Author: wangfpp
+* @Date:   2019-11-01 11:09:07
+* @Last Modified by:   wangfpp
+* @Last Modified time: 2019-11-01 11:09:57
+*/
