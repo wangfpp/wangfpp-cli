91a845dfcdf702bd41b3c846e6f29ab64da84943
wangfpp <wangfpp@gmail.com>
2019-11-07 18:43:17 +0800

    后续需要做的事情

--- js/create.js
+++ js/create.js
@@ -2,8 +2,10 @@
 * @Author: wangfpp
 * @Date:   2019-11-07 18:31:24
 * @Last Modified by:   wangfpp
-* @Last Modified time: 2019-11-07 18:35:07
+* @Last Modified time: 2019-11-07 18:43:07
 */
-module.exports = function(argv) {
-	console.log(argv);
+module.exports = function(app) {
+	// 这里执行 1. 创建项目
+	// 			2. 根据inquirer配置拉取项目模板
+	// 			3. npm install安装项目依赖
 }
